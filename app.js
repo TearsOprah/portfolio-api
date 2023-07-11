d8af429ef6ba517e331ff8fa965f605ccf1df319
TearsOprah <qmeshokp@mail.ru>
2023-07-11 14:10:26 +0300

    feat: возможность отдавать несколько изображения для одного проекта

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const path = require('path');
 const app = express();
 
 const projects = [
-  { id: 1, name: 'Проект 1', image: 'project1.jpg' },
-  { id: 2, name: 'Проект 2', image: 'project2.jpg' },
-  { id: 3, name: 'Проект 3', image: 'project3.jpg' }
+  {
+    id: 1,
+    name: 'Проект 1',
+    images: [
+      { id: 1, name: 'project1_1.jpg', path: 'project1_1.jpg' },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Проект 2',
+    images: [
+      { id: 1, name: 'project2_1.jpg', path: 'project2_1.jpg' },
+      { id: 2, name: 'project2_2.jpg', path: 'project2_2.jpg' }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Проект 3',
+    images: [
+      { id: 1, name: 'project3_1.jpg', path: 'project3_1.jpg' }
+    ]
+  }
 ];
 
 app.use(express.static('images'));
@@ -26,15 +45,33 @@ app.get('/projects/:id', (req, res) => {
   }
 });
 
-app.get('/projects/:id/image', (req, res) => {
+app.get('/projects/:id/images', (req, res) => {
   const projectId = parseInt(req.params.id);
   const project = projects.find(p => p.id === projectId);
 
-  if (project && project.image) {
-    const imagePath = path.join(__dirname, 'images', project.image);
-    res.sendFile(imagePath);
+  if (project && project.images) {
+    res.json(project.images);
   } else {
-    res.status(404).json({ error: 'Изображение не найдено' });
+    res.status(404).json({ error: 'Изображения не найдены' });
+  }
+});
+
+app.get('/projects/:id/images/:imageId', (req, res) => {
+  const projectId = parseInt(req.params.id);
+  const imageId = parseInt(req.params.imageId);
+  const project = projects.find(p => p.id === projectId);
+
+  if (project && project.images) {
+    const image = project.images.find(img => img.id === imageId);
+
+    if (image) {
+      const imagePath = path.join(__dirname, 'images', image.path);
+      res.sendFile(imagePath);
+    } else {
+      res.status(404).json({ error: 'Изображение не найдено' });
+    }
+  } else {
+    res.status(404).json({ error: 'Проект не найден' });
   }
 });
 

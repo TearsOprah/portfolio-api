f1696d385eb9094a368f90bcf1e3daed18298e7b
TearsOprah <qmeshokp@mail.ru>
2023-07-11 13:59:15 +0300

    feat: возможность отдавать изображения

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 const projects = [
-  { id: 1, name: 'Проект 1' },
-  { id: 2, name: 'Проект 2' },
-  { id: 3, name: 'Проект 3' }
-]
+  { id: 1, name: 'Проект 1', image: 'project1.jpg' },
+  { id: 2, name: 'Проект 2', image: 'project2.jpg' },
+  { id: 3, name: 'Проект 3', image: 'project3.jpg' }
+];
+
+app.use(express.static('images'));
 
 app.get('/projects', (req, res) => {
   res.json(projects);
@@ -19,6 +23,18 @@ app.get('/projects/:id', (req, res) => {
     res.json(project);
   } else {
     res.status(404).json({ error: 'Проект не найден' });
+  }
+});
+
+app.get('/projects/:id/image', (req, res) => {
+  const projectId = parseInt(req.params.id);
+  const project = projects.find(p => p.id === projectId);
+
+  if (project && project.image) {
+    const imagePath = path.join(__dirname, 'images', project.image);
+    res.sendFile(imagePath);
+  } else {
+    res.status(404).json({ error: 'Изображение не найдено' });
   }
 });
 
